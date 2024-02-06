e5bd591166e39c95b249c62e6dae8957167c22c5
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-02-06 22:02:24 +0100

    publish message to OrderReceived topic

---
+++ Order/cmd/publisher/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	topic := "OrderReceived"
+	for _, word := range []string{"Asynchronous", "Event", "Handling", "Using", "Microservices", "and", "Kafka"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	p.Flush(15 * 1000)
+}
--- Order/go.mod
+++ Order/go.mod
@@ -1,3 +1,5 @@
 module order.piotr-piotrowski.github.com
 
 go 1.21.6
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.3.0 // indirect
